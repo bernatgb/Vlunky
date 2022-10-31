2cb73017c0a273f480db7723b0f215f866a575a8
bernatgb <33035442+bernatgb@users.noreply.github.com>
2022-10-31 22:15:55 +0100

    Added Binari folder.

---
+++ Binari/levels/Map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Map" tilewidth="64" tileheight="64" tilecount="32" columns="32">
+ <image source="../images/Map.png" width="2048" height="64"/>
+</tileset>
